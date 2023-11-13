1f70154afd3a53ab541506ae079309d8f7af1301
jayjay2397 <jj.valdez09@gmail.com>
2023-11-13 15:50:24 -0600

    adding files to components folder

---
+++ client/src/components/AIChooser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AIChooser = () => {
+  return (
+    <div>AIChooser</div>
+  )
+}
+
+export default AIChooser
---
+++ client/src/components/ColorChooser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorChooser = () => {
+  return (
+    <div>ColorChooser</div>
+  )
+}
+
+export default ColorChooser
